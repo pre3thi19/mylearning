a9125e7d598c920affcb0b165b19b8073ded6bc8
pre3thi19 <preelaldr@gmail.com>
2025-08-13 23:09:03 +0530

    node changes

--- day1/node-env/server.js
+++ day1/node-env/server.js
@@ -1,15 +1,22 @@
 const http = require('http');
+const config = require('config');
+
+const port = config.get('port');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Environment Variables</h1><ul>');
-  for (const [key, value] of Object.entries(process.env)) {
-    res.write(`<li><b>${key}</b>: ${value}</li>`);
-  }
+
+  res.write('<h1>Config Values</h1><ul>');
+  res.write(`<li><b>app.name</b>: ${config.get('app.name')}</li>`);
+  res.write(`<li><b>app.environment</b>: ${config.get('app.environment')}</li>`);
+  res.write(`<li><b>db.host</b>: ${config.get('db.host')}</li>`);
+  res.write(`<li><b>db.port</b>: ${config.get('db.port')}</li>`);
+  res.write(`<li><b>port</b>: ${config.get('port')}</li>`);
   res.write('</ul>');
+
   res.end();
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
